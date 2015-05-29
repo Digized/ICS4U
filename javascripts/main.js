16a23de5e75df07532d520b607a279fd7c213bc2
Zuraiz Zafar <Digized@users.noreply.github.com>
2015-05-28 20:17:57 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,45 @@
 console.log('This would be the main JS file.');
-<!-- this script got from www.javascriptfreecode.com coded by: Krishna Eydat-->
-     <STYLE>BODY {
-SCROLLBAR-FACE-COLOR: red; SCROLLBAR-HIGHLIGHT-COLOR: gray; 
+<!-- this script got from www.javascriptfreecode.com-Coded by: Krishna Eydat -->
 
-SCROLLBAR-SHADOW-COLOR: black; SCROLLBAR-ARROW-COLOR: gray; 
-
-SCROLLBAR-TRACK-COLOR: black; SCROLLBAR-DARKSHADOW-COLOR: red
-}
-</STYLE>
+<p>[<a href="/"
+onmouseover="document.bgColor='green'">Green</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='greem'">Bright Green</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='seagreen'">Sea Green</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='red'">Red</a>]<BR>
+[<a href="/"
+onmouseover="document.bgColor='magenta'">Magenta</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='fusia'">Fusia</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='pink'">Pink</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='purple'">Purple</a>]<BR>
+[<a href="/"
+onmouseover="document.bgColor='navy'">Navy</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='blue'">Blue</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='royalblue'">Royal Blue</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='Skyblue'">Sky Blue</a>]<BR>
+[<a href="/"
+onmouseover="document.bgColor='yellow'">Yellow</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='brown'">Brown</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='almond'">Almond</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='white'">White</a>]<BR>
+[<a href="/"
+onmouseover="document.bgColor='black'">Black</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='coral'">Coral</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='olivedrab'">Olive Drab</a>]<br>
+[<a href="/"
+onmouseover="document.bgColor='orange'">Orange</a>]<br>
+<hr color="#00FFFF">
+<font face="Tahoma"><a target="_blank" href="http://www.javascriptfreecode.com/"><span style="font-size: 8pt; text-decoration: none">JavaScript Free Code</span></a></font>
