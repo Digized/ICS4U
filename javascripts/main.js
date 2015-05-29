77250c18ef2df8e3eb63833026abcd14bb67c802
Zuraiz Zafar <Digized@users.noreply.github.com>
2015-05-28 20:21:37 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,113 @@
 console.log('This would be the main JS file.');
-<!-- this script got from www.javascriptfreecode.com coded by: Krishna Eydat-->
-<body style="background-color: #3e4b6a">
-<font color="#9ba5c7">
-<script language="javascript">
-document.write(unescape("%3CDIV%20id%3DLayer1%20style%3D%22Z-INDEX%3A1%3B%20LEFT%3A0px%3B%20WIDTH%3A0%3B%20POSITION%3Aabsolute%3B%20TOP%3A0px%3B%20HEIGHT%3A0%3B%22%3E%0D%0A%3CEMBED%20pluginspage%3D%22http%3A//www.macromedia.com/go/getflashplayer%22%20src%3D%22http://bahar-20.com/ftp/subject/other.best/java.ghatreh/drip.swf%22%20width%3D%22990%22%20height%3D%22640%22%20type%3Dapplication/x-shockwave-flash%20quality%3D%22high%22%20menu%3D%22false%22%20wmode%3D%22transparent%22%3E%0D%0A%3C/EMBED%3E%0D%0A%3C/DIV%3E%0D%0A%3CDIV%20id%3DLayer1%20style%3D%22Z-INDEX%3A1%3B%20LEFT%3A0px%3B%20WIDTH%3A0%3B%20POSITION%3Aabsolute%3B%20TOP%3A645px%3B%20HEIGHT%3A0%3B%22%3E%0D%0A%3CEMBED%20pluginspage%3D%22http%3A//www.macromedia.com/go/getflashplayer%22%20src%3D%22http://bahar-20.com/ftp/subject/other.best/java.ghatreh/drip.swf%22%20width%3D%22990%22%20height%3D%22640%22%20type%3Dapplication/x-shockwave-flash%20quality%3D%22high%22%20menu%3D%22false%22%20wmode%3D%22transparent%22%3E%0D%0A%3C/EMBED%3E%0D%0A%3C/DIV%3E"));
+<!-- this script got from www.javascriptfreecode.com-Coded by: Krishna Eydatoula -->
+<script>
+// Set the number of snowflakes (more than 30 - 40 not recommended)
+var snowmax=35
+
+// Set the colors for the snow. Add as many colors as you like
+var snowcolor=new Array("#aaaacc","#ddddFF","#ccccDD")
+
+// Set the fonts, that create the snowflakes. Add as many fonts as you like
+var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
+
+// Set the letter that creates your snowflake (recommended:*)
+var snowletter="*"
+
+// Set the speed of sinking (recommended values range from 0.3 to 2)
+var sinkspeed=0.6
+
+// Set the maximal-size of your snowflaxes
+var snowmaxsize=22
+
+// Set the minimal-size of your snowflaxes
+var snowminsize=8
+
+// Set the snowing-zone
+// Set 1 for all-over-snowing, set 2 for left-side-snowing 
+// Set 3 for center-snowing, set 4 for right-side-snowing
+var snowingzone=3
+
+///////////////////////////////////////////////////////////////////////////
+// CONFIGURATION ENDS HERE
+///////////////////////////////////////////////////////////////////////////
+
+
+// Do not edit below this line
+var snow=new Array()
+var marginbottom
+var marginright
+var timer
+var i_snow=0
+var x_mv=new Array();
+var crds=new Array();
+var lftrght=new Array();
+var browserinfos=navigator.userAgent 
+var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)
+var ns6=document.getElementById&&!document.all
+var opera=browserinfos.match(/Opera/)  
+var browserok=ie5||ns6||opera
+
+function randommaker(range) {		
+	rand=Math.floor(range*Math.random())
+    return rand
+}
+
+function initsnow() {
+	if (ie5 || opera) {
+		marginbottom = document.body.clientHeight
+		marginright = document.body.clientWidth
+	}
+	else if (ns6) {
+		marginbottom = window.innerHeight
+		marginright = window.innerWidth
+	}
+	var snowsizerange=snowmaxsize-snowminsize
+	for (i=0;i<=snowmax;i++) {
+		crds[i] = 0;                      
+    	lftrght[i] = Math.random()*15;         
+    	x_mv[i] = 0.03 + Math.random()/10;
+		snow[i]=document.getElementById("s"+i)
+		snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]
+		snow[i].size=randommaker(snowsizerange)+snowminsize
+		snow[i].style.fontSize=snow[i].size
+		snow[i].style.color=snowcolor[randommaker(snowcolor.length)]
+		snow[i].sink=sinkspeed*snow[i].size/5
+		if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+		if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+		if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+		if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+		snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)
+		snow[i].style.left=snow[i].posx
+		snow[i].style.top=snow[i].posy
+	}
+	movesnow()
+}
+
+function movesnow() {
+	for (i=0;i<=snowmax;i++) {
+		crds[i] += x_mv[i];
+		snow[i].posy+=snow[i].sink
+		snow[i].style.left=snow[i].posx+lftrght[i]*Math.sin(crds[i]);
+		snow[i].style.top=snow[i].posy
+		
+		if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i])){
+			if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+			if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+			if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+			if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+			snow[i].posy=0
+		}
+	}
+	var timer=setTimeout("movesnow()",50)
+}
+
+for (i=0;i<=snowmax;i++) {
+	document.write("<span id='s"+i+"' style='position:absolute;top:-"+snowmaxsize+"'>"+snowletter+"</span>")
+}
+if (browserok) {
+	window.onload=initsnow
+}
 </script>
-<div style="Z-INDEX:1; LEFT:0px; WIDTH:0; POSITION:absolute; TOP:0px; HEIGHT:0;" id="Layer1">
-<embed width="990" height="640" wmode="transparent" menu="false" quality="high" type="application/x-shockwave-flash" src="http://bahar-20.com/ftp/subject/other.best/java.ghatreh/drip.swf" pluginspage="http://www.macromedia.com/go/getflashplayer">
-</div>
-<div style="Z-INDEX:1; LEFT:0px; WIDTH:0; POSITION:absolute; TOP:645px; HEIGHT:0;" id="Layer1">
-<embed width="990" height="640" wmode="transparent" menu="false" quality="high" type="application/x-shockwave-flash" src="http://bahar-20.com/ftp/subject/other.best/java.ghatreh/drip.swf" pluginspage="http://www.macromedia.com/go/getflashplayer">
-</div><br><br><br>
-</font>
-</body>
+
+
 <font face="Tahoma"><a target="_blank" href="http://www.javascriptfreecode.com/"><span style="font-size: 8pt; text-decoration: none">JavaScript Free Code</span></a></font>
